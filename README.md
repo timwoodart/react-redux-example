e59ab622f185c31b7a029e247a425df225bb741c
Timothy Wood <timwoodwood@gmail.com>
2016-02-17 16:28:22 -0500

    Setup intake form and amke stateful

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 #react-flux-example
 Example repo and boilerplate for React using Flux Architecture.
+
+TODO:
+Set up intake form
+Set application state
+output user details
+
--- app/components/Profile/ProfileForm.js
+++ app/components/Profile/ProfileForm.js
@@ -1,39 +1,82 @@
-import React from 'react';
-import {Input} from 'react-bootstrap';
+import React, {PropTypes} from 'react';
+import {findDOMNode} from 'react-dom';
+import {Input, Button} from 'react-bootstrap';
 import {Grid, Row, Column} from 'react-cellblock';
 
 export default class ProfileForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    
+    // default state
+    this.state = {
+      'firstName': '',
+      'lastName': '',
+      'title': '',
+      'city': ''
+    }
+
+    this._submitProfile = this._submitProfile.bind(this);
+    this._updateInput = this._updateInput.bind(this);
+  }
+  
+  _submitProfile(e) {
+    e.preventDefault();
+    // TODO - dispatch values to update state tree
+  }
+
+  _updateInput(inputName, e) {
+    // es6 computed property names for inputName
+    this.setState({[inputName]: e.target.value});
+  }
+
   render() {
     return (
 
       <Grid>
-        <h1>Add your Info</h1>
+        <h1>{this.props.formHeading}</h1>
         <Row>
           <Column width="1/2">
-              <form className="profile-form">
+              <form ref="profileForm" className="profile-form">
                 <fieldset>
-                  <Input 
+                  <Input
                     type="text"
                     label="First Name"
                     placeholder="Enter your First Name"
+                    onChange={this._updateInput.bind(null, 'firstName')}
+                    value={this.state.firstName}
                   />
                   <Input 
+                    id="lastName"
                     type="text"
                     label="Last Name"
                     placeholder="Enter your Last Name"
+                    onChange={this._updateInput.bind(null, 'lastName')}
+                    value={this.state.lastName}
                   />
                   <Input 
                     type="text"
                     label="Title"
                     placeholder="Enter your Professional Title"
+                    onChange={this._updateInput.bind(null, 'title')}
+                    value={this.state.title}
                   />
                   <Input 
                     type="text"
                     label="City"
                     placeholder="Enter the City you live in"
+                    onChange={this._updateInput.bind(null, 'city')}
+                    value={this.state.city}
                   />
                 </fieldset>
+                <Button
+                  bsSize='large'
+                  bsStyle='primary'
+                  onClick={this._submitProfile}>
+                    Submit
+                </Button>
               </form>
+
           </Column>
           <Column width="1/2">
             Your Profile:
@@ -42,4 +85,12 @@ export default class ProfileForm extends React.Component {
       </Grid>
     );
   }
+}
+
+ProfileForm.propTypes = {
+  formHeading: PropTypes.String
+}
+
+ProfileForm.defaultProps = {
+  formHeading: 'Start by adding your info'
 }
